8aabe2459adb4b3427211fe319ed445c8ed0d04d
Ross <Rheaney26@Gmail.com>
2022-08-24 21:49:28 +0100

    first commit

---
+++ helloworld.nim
@@ -0,0 +1,3 @@
+echo "what is your name?"
+var name: string = readLine(stdin)
+echo "hi,", name, "!"
---
+++ test.nim
@@ -0,0 +1,5 @@
+# Basically I just need to do the following
+# 1) Create a procedure
+# 2) Read in user input 
+
+
